277a66b348dbf870c101daee142e562bde491741
Sangwoo (Thomas) Hahn <thahn106@gmail.com>
2023-05-14 19:28:10 +0900

    new: configure tensorflow with GPU

--- Model.py
+++ Model.py
@@ -1,3 +1,19 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     custom_cell_magics: kql
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.11.2
+#   kernelspec:
+#     display_name: env
+#     language: python
+#     name: python3
+# ---
+
 # %% [markdown]
 # # Model
 #
@@ -11,33 +27,26 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 
-# import data
-train_file = os.path.join(os.getcwd(), "data", "train.csv")
-# test_file = os.path.join(os.getcwd(), "data", "test.csv")
-
-train_data_raw = pd.read_csv(train_file, parse_dates=["IssueDateTime"])
-# test_data_raw = pd.read_csv(test_file, parse_dates=["IssueDateTime"])
-
-# %%
 # Verify Tensorflow is installed correctly
-# https://chancoding.tistory.com/223
 from tensorflow import keras
 from tensorflow.python.client import device_lib
 
 print(f"{tf.__version__=}, {keras.__version__=}")
 
 # Verify GPU is available
-device_lib.list_local_devices()
+print(device_lib.list_local_devices())
 
+# import data
+train_file = os.path.join(os.getcwd(), "data", "train.csv")
+test_file = os.path.join(os.getcwd(), "data", "test.csv")
+
+train_data_raw = pd.read_csv(train_file, parse_dates=["IssueDateTime"])
+test_data_raw = pd.read_csv(test_file, parse_dates=["IssueDateTime"])
 
 # %%
 # Preprocessing
 
+
 def preprocess(data_raw):
     data = data_raw.copy()
-    
     return data
-
-
-
-# %%
